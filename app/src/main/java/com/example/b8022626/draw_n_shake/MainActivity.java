758549d0c7ea6e4d63c0b12a4f0be0599b213909
Josh <33764106+joshsj@users.noreply.github.com>
2019-03-01 10:33:36 +0000

    Add project structure

---
+++ app/src/main/java/com/example/b8022626/draw_n_shake/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.b8022626.draw_n_shake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
