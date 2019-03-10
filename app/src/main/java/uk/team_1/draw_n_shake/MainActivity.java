0b9681f802bd840483857ae0b83121af3038dd26
joshsj <33764106+joshsj@users.noreply.github.com>
2019-03-10 01:20:13 +0000

    Configure for AS3, rename package, remove unit tests

--- app/src/main/java/uk/team_1/draw_n_shake/MainActivity.java
+++ app/src/main/java/uk/team_1/draw_n_shake/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.b8022626.draw_n_shake;
+package uk.team_1.draw_n_shake;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import uk.team_1.draw_n_shake.R;
 
 public class MainActivity extends AppCompatActivity {
 
